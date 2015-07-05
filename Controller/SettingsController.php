0f0b21ca194d8ccb5a6ca5e50b76c3b36776ff92
ツ Liverbool <nukboon@gmail.com>
2015-07-05 17:04:51 +0700

    remove dos-controller.

--- Controller/SettingsController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace DoS\SettingsBundle\Controller;
-
-use Sylius\Bundle\SettingsBundle\Controller\SettingsController as BaseSettingsController;
-
-class SettingsController extends BaseSettingsController
-{
-}
